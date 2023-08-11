2eaff589701115abcb1f663eceed3f98d1cf2825
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-11 19:14:47 +1000

    Update StreakManager.php

--- src/antbag/votestreak/Manager/StreakManager.php
+++ src/antbag/votestreak/Manager/StreakManager.php
@@ -1,1 +1,12 @@
 <?php
+
+namespace antbag\VoteStreak;
+use antbag\VoteStrek\Listeners\Voting38Listener;
+final class StreakManager{
+public function AddStreak();
+  $v38 = $this->Voting38Listener();
+  $voteevent = $v38()->onVote();
+  $player = $voteevent->$player();
+
+
+}
