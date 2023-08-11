c28c646df84367e20f7682539326bd474ba604d6
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-11 19:09:12 +1000

    Update Voting38Listener.php

--- src/antbag/votestreak/Listeners/Voting38Listener.php
+++ src/antbag/votestreak/Listeners/Voting38Listener.php
@@ -3,10 +3,14 @@
 namespace antbag\votestreak\Listeners;
 
 use kingofturkey38\voting38\events\PlayerVoteEvent;
-
+use antbag\VoteStreak\manager\StreakManager;
 class Voting38Listener implements Listener {
   
   public function onVote(PlayerVoteEvent $event) {
+   $player = $event()->getPlayer();
+    $addstreak = $this->StreakManager();
+    $player->addStreak(1);
     
   }
+  
 }
