fdd62695349a6850057a597002d8922ad7a4e56d
antbag-dev <abdulrhemanrashid@gmail.com>
2023-08-11 13:31:18 +0500

    added votelistner

--- src/antbag/votestreak/Main.php
+++ src/antbag/votestreak/Main.php
@@ -6,4 +6,14 @@ use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase implements Listener {
   
+  private $Voting38 = false;
+  
+  public function onEnable(): void {
+    
+    if($this->getServer()->getPluginManager()->getPlugin("Voting38") != null && $this->getConfig()->get("Voting38") == true) {
+      $this->Voting38 = true;
+      $this->getServer()->getPluginManager()->registerEvents(new Voting38Listener($this), $this);
+      
+    }
+  }
 }
