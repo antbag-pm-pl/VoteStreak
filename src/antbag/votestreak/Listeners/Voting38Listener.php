3c671671ff29359af504eb1234f275a00ff5e0a6
antbag-dev <abdulrhemanrashid@gmail.com>
2023-08-11 13:48:10 +0500

    create voting 38

---
+++ src/antbag/votestreak/Listeners/Voting38Listener.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace antbag\votestreak\Listeners;
+
+use kingofturkey38\voting38\events\PlayerVoteEvent;
+
+class Voting38Listener implements Listener {
+  
+  public function onVote(PlayerVoteEvent $event) {
+    
+  }
+}
