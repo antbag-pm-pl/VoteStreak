218abb35defab84baaa4e4be932f1e10d238f87d
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-11 19:20:58 +1000

    Update StreakManager.php

--- src/antbag/votestreak/Manager/StreakManager.php
+++ src/antbag/votestreak/Manager/StreakManager.php
@@ -3,11 +3,13 @@
 namespace antbag\VoteStreak;
 use antbag\VoteStrek\Listeners\Voting38Listener;
 final class StreakManager{
-  
 public function AddStreak();
   $v38 = $this->Voting38Listener();
   $voteevent = $v38()->onVote();
   $player = $voteevent->$player();
-
+$playerdata = $this->getDataFolder("PlayerData" . YAML);
+  $streak = $playerdata->get("Streak");
+  $data = $player->getUUID(); . $streak 
+  $playerdata->file_put_Contents($data);
 
 }
