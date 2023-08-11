8648834c75db6662f0a6d35d6a69350312e5cc1f
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-11 19:26:10 +1000

    Update StreakManager.php
    Todo more

--- src/antbag/votestreak/Manager/StreakManager.php
+++ src/antbag/votestreak/Manager/StreakManager.php
@@ -2,14 +2,38 @@
 
 namespace antbag\VoteStreak;
 use antbag\VoteStrek\Listeners\Voting38Listener;
+use DateTime;
 final class StreakManager{
-public function AddStreak();
+public function AddStreak(){
   $v38 = $this->Voting38Listener();
   $voteevent = $v38()->onVote();
+  $DateTime = $this->DateTime();
   $player = $voteevent->$player();
 $playerdata = $this->getDataFolder("PlayerData" . YAML);
   $streak = $playerdata->get("Streak");
-  $data = $player->getUUID(); . $streak 
+  $data = $player->getUUID(); . $streak . $DateTime->TimeNow();
   $playerdata->file_put_Contents($data);
 
 }
+ public function SetNil(){
+$playerdata = $this->getDataFolder("PlayerData" . YAML);
+  $streak = $playerdata->get("Streak");
+   $DateTime = $this->DateTime();
+  $data = $player->getUUID(); . "0" .  $DateTime->TimeNow();
+   
+
+// your first date coming from a mysql database (date fields)
+
+/* 3rd argument of data$dateA = $data->[3]; 
+// your second date coming from a mysql database (date fields)
+
+    $dateB = '2013-11-11 16:27:21'; 
+
+    $timediff = strtotime($dateA) - strtotime($dateB);
+
+if($timediff > 86400){
+   
+  $playerdata->file_put_Contents($data);
+ */
+}
+
