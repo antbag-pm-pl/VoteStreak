2daa9a738b4cc55adb70b41344e0eb5a85073cef
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-13 18:35:06 +1000

    Update JoinListener.php

--- src/antbag/votestreak/Listeners/JoinListener.php
+++ src/antbag/votestreak/Listeners/JoinListener.php
@@ -2,13 +2,13 @@
 
 namespace antbag\votestreak;
 
-use pocketmine\event\PlayerJoinEvent;
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\Listener;
 use antbag\votestreak\Manager\StreakManager;
 use DateTime;
 
 class JoinListener implements Listener{
-public function onJoin($event PlayerJoinEvent): void {
+public function onJoin(PlayerJoinEvent $event): void {
 $player = $event->getPlayer();
   $ptime = $player->getCurrentStreak();
   $timeNow = $this->DateTime()->timeNow();
