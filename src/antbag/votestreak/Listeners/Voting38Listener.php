bbb81dc62c0f9916388b164032cf4452af2f1161
notant <84870445+antbag-dev@users.noreply.github.com>
2023-09-21 13:11:35 +0500

    I was on drugs

--- src/antbag/votestreak/Listeners/Voting38Listener.php
+++ src/antbag/votestreak/Listeners/Voting38Listener.php
@@ -5,7 +5,7 @@ namespace antbag\votestreak\Listeners;
 use kingofturkey38\voting38\events\PlayerVoteEvent;
 use antbag\votestreak\Main;
 
-class Voting38Listener extends Listener {
+class Voting38Listener implements Listener {
   
   
   public function onVote(PlayerVoteEvent $event) {
